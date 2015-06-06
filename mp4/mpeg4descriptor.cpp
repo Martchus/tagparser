c86103829c2b43c21326eadde542bcd448ec1f4f
Martchus <martchus@gmx.net>
2015-06-07 00:16:59 +0200

    ignore docs generated by Doxygen

---
+++ mp4/mpeg4descriptor.cpp
@@ -0,0 +1,7 @@
+#include "mpeg4streamdescriptor.h"
+
+Mpeg4StreamDescriptor::Mpeg4StreamDescriptor()
+{
+
+}
+
---
+++ mp4/mpeg4descriptor.h
@@ -0,0 +1,11 @@
+#ifndef MPEG4STREAMDESCRIPTOR_H
+#define MPEG4STREAMDESCRIPTOR_H
+
+
+class Mpeg4StreamDescriptor
+{
+public:
+    Mpeg4StreamDescriptor();
+};
+
+#endif // MPEG4STREAMDESCRIPTOR_H
